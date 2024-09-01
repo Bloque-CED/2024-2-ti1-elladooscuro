072ed6472ebadf78eb025256e8bca5d2c20478cf
5corpi0n <1108829549@u.icesi.edu.co>
2024-09-01 15:26:44 -0500

    Pruebas HeapSort

---
+++ Ordenamiento/src/main/scala/co/com/u/icesi/Main.scala
@@ -0,0 +1,5 @@
+package co.com.u.icesi
+  
+  def heapSort(a: Array[Int]): Array[Int] = {
+    a
+  }
---
+++ Ordenamiento/src/test/scala/co/com/u/icesi/MainTest.scala
@@ -0,0 +1,72 @@
+package co.com.u.icesi
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class MainTest extends AnyFunSuite {
+
+
+  //HeapSort -> del cap 6 text de Cormen
+  test("Normal case") {
+    val a = Array(4, 1, 3, 2, 16, 9, 10, 14, 8, 7)
+    val b = Array(1, 2, 3, 4, 7, 8, 9, 10, 14, 16)
+
+    val result = heapSort(a)
+    assert(result === b)
+
+  }
+
+  // Test case: Arrangement already ordered
+  test("Arrangement already ordered") {
+    val a = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val result = heapSort(a)
+    assert(result === a)
+  }
+
+  // Test Case: Arrangement in descending order
+  test("Test Case: Arrangement in descending order") {
+    val a = Array(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+    val b = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val result = heapSort(a)
+    assert(result === b)
+  }
+
+  // Test case: Arrangement with repeated elements
+  test("Arrangement with repeated elements") {
+    val a = Array(4, 10, 3, 10, 2, 10, 1, 4)
+    val b = Array(1, 2, 3, 4, 4, 10, 10, 10)
+    val result = heapSort(a)
+    assert(result === b)
+  }
+
+  //  Test case: Single-element array
+  test("Single-element array") {
+    val a = Array(5)
+    val result = heapSort(a)
+    assert(result === a)
+  }
+
+  //Test case: Empty arrangement
+  test("Empty arrangement") {
+    val a = Array[Int]()
+    val result = heapSort(a)
+    assert(result.isEmpty)
+  }
+
+  // Test case: Arrangement with all elements the same
+  test("Arrangement with all elements the same") {
+    val a = Array(5, 5, 5, 5, 5, 5, 5)
+    val result = heapSort(a)
+    assert(result === a)
+  }
+
+
+
+
+  //CountingSort , de acuerdo a la descripción del capítulo 8 del texto de Cormen[1]
+  //RadixSort , de acuerdo a la descripción del capítulo8 del texto de Cormen[1]
+
+
+
+
+
+}
